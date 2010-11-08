5f4389cbbe82dd0b3cc6a719b687f8c1c9f4a7b2
Julien Cheype <cheype@gmail.com>
2010-11-08 12:30:53 +0100

    add first demo app

---
+++ demo/main.js
@@ -0,0 +1,25 @@
+include("util.js");
+
+// SET a random led color
+var randColor = function(){
+	var rand = "" + Math.random();
+	var color = rand.substr(2, 6);
+	log("light: " +  color);
+  karotz.led.light(color);
+  return true;
+}
+
+//Callback function
+var onKarotzConnect = function(data){
+  
+  // Add a button event listener and call "randColor" on simple click
+  karotz.addButtonListener(function(event){
+  	log("button: " + event);
+  	if(event == "SIMPLE")
+  	  randColor()
+  	return true;
+  });
+}
+
+//Connect to Karotz and call "onKarotzConnect" callback
+karotz.connectAndStart("192.168.1.123", 9123, onKarotzConnect, {});
